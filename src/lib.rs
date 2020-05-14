331a816a4191f9ba6aef6093109f946ee14e6a42
Ryan Levick <ryan.levick@gmail.com>
2020-05-14 16:11:34 +0200

    Set up challenge

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+struct NumArray {}
+
+impl NumArray {
+    pub fn new(mut nums: Vec<i32>) -> Self {
+        todo!()
+    }
+
+    pub fn sum_range(&self, i: i32, j: i32) -> i32 {
+        todo!()
+    }
+}
